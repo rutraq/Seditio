6aa9c206a3da02a82c8c668bf054c716a2be9844
rutraq <a3332133@gmail.com>
2022-02-03 10:39:46 +0100

    page scrolling

--- script/mainPage.js
+++ script/mainPage.js
@@ -1,8 +1,16 @@
 $(window).bind('mousewheel DOMMouseScroll', function(event){
     if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-
+        window.scrollTo({
+            top: -window.innerHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
     }
     else {
-
+        window.scrollTo({
+            top: window.innerHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
     }
 });
