bea73ae3db2812ab82b444550bac32a0e3c02feb
rutraq <a3332133@gmail.com>
2022-02-04 13:16:36 +0100

    2nd page animation

--- script/mainPage.js
+++ script/mainPage.js
@@ -5,6 +5,7 @@ $(window).bind('mousewheel DOMMouseScroll', function(event){
             left: 0,
             behavior: 'smooth'
         });
+        $("#rightBubble").animate({opacity: '1'}, 500);
     }
     else {
         window.scrollTo({
@@ -12,5 +13,46 @@ $(window).bind('mousewheel DOMMouseScroll', function(event){
             left: 0,
             behavior: 'smooth'
         });
+        $(".header").addClass("fix");
+        $("#rightBubble").animate({opacity: '0'}, 500);
+        $("#centerBubble").addClass('rotation');
+        $("#nftImg").animate({
+            opacity: 1,
+            top: '15%'
+        }, 1500);
+
+        $("#smart").animate({
+            right: '20%',
+            opacity: 1
+        }, 1500);
+        $("#future").animate({
+            right: '27%',
+            opacity: 1
+        }, 1500);
+        $("#new").animate({
+            right: '15%',
+            opacity: 1
+        }, 1500);
+        $("#trends").animate({
+            right: '29%',
+            opacity: 1
+        }, 1500);
+
+        $("#seditio").animate({
+            top: '30%',
+            opacity: 1
+        }, 1200);
+        $("#text").animate({
+            top: '47%',
+            opacity: 1
+        }, 1200);
+        $("#line").animate({
+            top: '47%',
+            opacity: 1
+        }, 1200);
+        $("#readMore").animate({
+            top: '55%',
+            opacity: 1
+        }, 1200);
     }
 });
